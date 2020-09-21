75b3e01f2e34321ca541a45c91ae8e2166e0bc7f
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-09-21 10:32:46 -0300

    21.09.2020_10:32

--- XF_Crud_SQLite/XF_Crud_SQLite/App.xaml.cs
+++ XF_Crud_SQLite/XF_Crud_SQLite/App.xaml.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XF_Crud_SQLite.Pages;
+using XF_Crud_SQLite.Persistencia;
 
 namespace XF_Crud_SQLite
 {
     public partial class App : Application
     {
+        static AcessoDB dbContext;
+
+        public static AcessoDB DataBase
+        {
+            get
+            {
+                if (dbContext == null)
+                    dbContext = new AcessoDB();
+
+                return dbContext;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new CadastroPage());
         }
 
         protected override void OnStart()
--- XF_Crud_SQLite/XF_Crud_SQLite/Pages/IncluirPage.xaml.cs
+++ XF_Crud_SQLite/XF_Crud_SQLite/Pages/IncluirPage.xaml.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XF_Crud_SQLite.Model;
+using XF_Crud_SQLite.Persistencia;
 
 namespace XF_Crud_SQLite.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class IncluirPage : ContentPage
     {
-        public IncluirPage()
+        //AcessoDB accdb = new AcessoDB();
+        ObservableCollection<Usuario> colecaoUsuarios;
+
+        public IncluirPage(ObservableCollection<Usuario> _usuarios)
         {
+            colecaoUsuarios = _usuarios;
             InitializeComponent();
+        }
+
+        private void Salvar_Clicked(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = txtNome.Text;
+            usuario.Email = txtEmail.Text;
+            usuario.Ativo = bAtivo.On;
+
+            //accdb.InserirUsuario(usuario);
+            App.DataBase.InserirUsuario(usuario);
+
+            colecaoUsuarios.Add(usuario);
+            Navigation.PopAsync();
+        }
+
+        private void Cancelar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
         }
     }
 }
+
